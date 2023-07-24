b09ea5f9debaaa8b9ba2d312fa9c701f0dcc15d9
Peter Njiru <peternjirum@gmail.com>
2023-07-23 21:52:13 -0400

    completes inheritance lab part 1

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name,  knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, string):
+        self.string  = string
+        return self.knowledge.append(self.string)
+    
+my_student = Student("My", "Student")
+my_student.learn("New information")
